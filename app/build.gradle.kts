7972936cab639b27ae4b4ef6fa9a95cecb01b787
P.K.M.Wijawardana <68194744+ManeeGitHubAccount@users.noreply.github.com>
2024-06-06 00:11:06 +0530

    Bottom Nav bar create

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,11 @@ android {
     }
 }
 
+
 dependencies {
+
+
+
 
     implementation(libs.appcompat)
     implementation(libs.material)
@@ -41,3 +45,4 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
