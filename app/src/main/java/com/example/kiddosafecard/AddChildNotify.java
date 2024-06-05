2d7aa9ac793aeced28191cf46c06ebcedeee34c5
P.K.M.Wijawardana <68194744+ManeeGitHubAccount@users.noreply.github.com>
2024-06-06 02:47:02 +0530

    Add Child Screen Finished Check the other Screens

---
+++ app/src/main/java/com/example/kiddosafecard/AddChildNotify.java
@@ -0,0 +1,12 @@
+package com.example.kiddosafecard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddChildNotify extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_child_notify);
+    }
+}
---
+++ app/src/main/java/com/example/kiddosafecard/BottomNavBarActivity.java
@@ -0,0 +1,12 @@
+package com.example.kiddosafecard;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BottomNavBarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_nav);
+    }
+}
