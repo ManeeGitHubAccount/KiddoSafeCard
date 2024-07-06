eb29e7a5d3859dffd64b6123915fb888406cb332
P.K.M.Wijawardana <68194744+ManeeGitHubAccount@users.noreply.github.com>
2024-07-06 13:54:42 +0530

    changes of some screens

--- app/src/main/java/com/example/kiddosafecard/Dashboard.java
+++ app/src/main/java/com/example/kiddosafecard/Dashboard.java
@@ -3,7 +3,7 @@ package com.example.kiddosafecard;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
-import com.example.kiddosafecard.R;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +14,22 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.dashboard);
 
         ImageButton registerChildButton = findViewById(R.id.img_btn_reg_child);
+        ImageButton immunizationButton = findViewById(R.id.btn_img_immunization);
+        TextView immunizationTextView = findViewById(R.id.txt_immunization);
 
         registerChildButton.setOnClickListener(v -> {
             Intent intent = new Intent(Dashboard.this, AddChildActivity.class);
             startActivity(intent);
         });
 
+        immunizationButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Dashboard.this, ImmunizationActivity.class);
+            startActivity(intent);
+        });
+
+        immunizationTextView.setOnClickListener(v -> {
+            Intent intent = new Intent(Dashboard.this, ImmunizationActivity.class);
+            startActivity(intent);
+        });
     }
 }
