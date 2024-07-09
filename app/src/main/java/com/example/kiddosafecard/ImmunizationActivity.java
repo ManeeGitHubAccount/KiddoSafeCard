73be7d096284d4a403f0df4afef244430305b608
P.K.M.Wijawardana <68194744+ManeeGitHubAccount@users.noreply.github.com>
2024-07-09 16:26:32 +0530

    changes of some screens

--- app/src/main/java/com/example/kiddosafecard/ImmunizationActivity.java
+++ app/src/main/java/com/example/kiddosafecard/ImmunizationActivity.java
@@ -1,12 +1,28 @@
 package com.example.kiddosafecard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ImmunizationActivity extends AppCompatActivity {
+
+    private Button btnHistory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.immunization); // Make sure to create immunization.xml layout
+
+        btnHistory = findViewById(R.id.btn_history);
+        btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to ImmunizationHistoryActivity
+                Intent intent = new Intent(ImmunizationActivity.this, ImmunizationHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
