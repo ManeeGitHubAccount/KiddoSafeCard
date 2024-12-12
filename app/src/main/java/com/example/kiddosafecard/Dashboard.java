1d877895e87662c1a15252dd7a9971d88158a0ba
P.K.M.Wijawardana <68194744+ManeeGitHubAccount@users.noreply.github.com>
2024-12-13 00:47:34 +0530

    Logout UI Finished

--- app/src/main/java/com/example/kiddosafecard/Dashboard.java
+++ app/src/main/java/com/example/kiddosafecard/Dashboard.java
@@ -2,6 +2,7 @@ package com.example.kiddosafecard;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +10,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.kiddosafecard.fragments.ChildFragment;
 
 public class Dashboard extends AppCompatActivity {
+
+    ImageButton btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        btn = findViewById(R.id.btn_logout);
+
+        btn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(Dashboard.this,LogoutActivity.class));
+            }
+        });
 
         ImageButton registerChildButton = findViewById(R.id.img_btn_reg_child);
         ImageButton immunizationButton = findViewById(R.id.btn_img_immunization);
--- app/src/main/java/com/example/kiddosafecard/LogoutActivity.java
+++ app/src/main/java/com/example/kiddosafecard/LogoutActivity.java
@@ -1,13 +1,19 @@
 package com.example.kiddosafecard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogoutActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logoutscreen);
+
     }
 }
